18054216b7a6471c1da00ce12b726899a4a49a84
BlakeeBobz <123bobisgaming@gmail.com>
2025-03-17 12:24:30 -0400

    Create Echo.bat

---
+++ Echo.bat
@@ -0,0 +1,8 @@
+@echo off
+::start code
+color 02
+::makes console green with black background
+echo Hello There
+::shows "Hello There" in the console
+pause
+::requires key press before closing window
