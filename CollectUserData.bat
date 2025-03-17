152e3703c0062e6f096f26c6ebec4ad853fa33da
BlakeeBobz <123bobisgaming@gmail.com>
2025-03-17 12:32:16 -0400

    Create CollectUserData.bat

---
+++ CollectUserData.bat
@@ -0,0 +1,12 @@
+@echo off
+echo %USERNAME%
+::get username
+echo %COMPUTERNAME%
+::get computer name
+echo %CD%
+::current directory path
+dir
+::get directory data
+tasklist
+::get current processes
+pause
