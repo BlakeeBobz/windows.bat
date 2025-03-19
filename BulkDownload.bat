a5d8c1c1b0aa9da4dc1c1f980200773e19d26939
BlakeeBobz <123bobisgaming@gmail.com>
2025-03-19 07:51:32 -0400

    Update BulkDownload.bat

--- BulkDownload.bat
+++ BulkDownload.bat
@@ -2,14 +2,14 @@
 echo Hi
 pause
 cls
-powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/BlakeeBobz/windows.bat/main/' -OutFile 'CollectUserData.bat'}"
+powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/BlakeeBobz/windows.bat/main/' -OutFile 'CollectUserData.bat'}" -Headers @{'User-Agent'='Mozilla/5.0'}}"
 echo Downloaded CollectUserData.bat.
-powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/BlakeeBobz/windows.bat/main/' -OutFile 'Echo.bat'}"
+powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/BlakeeBobz/windows.bat/main/' -OutFile 'Echo.bat'}" -Headers @{'User-Agent'='Mozilla/5.0'}}"
 echo Downloaded Echo.bat.
-powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/BlakeeBobz/windows.bat/main/' -OutFile 'GetInputAsVariable.bat'}"
+powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/BlakeeBobz/windows.bat/main/' -OutFile 'GetInputAsVariable.bat'}" -Headers @{'User-Agent'='Mozilla/5.0'}}"
 echo Downloaded GetInputAsVariable.bat.
-powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/BlakeeBobz/windows.bat/main/' -OutFile 'GetName.bat'}"
+powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/BlakeeBobz/windows.bat/main/' -OutFile 'GetName.bat'}" -Headers @{'User-Agent'='Mozilla/5.0'}}"
 echo Downloaded GetName.bat.
-powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/BlakeeBobz/windows.bat/main/' -OutFile 'RandomNumber.bat'}"
+powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/BlakeeBobz/windows.bat/main/' -OutFile 'RandomNumber.bat'}" -Headers @{'User-Agent'='Mozilla/5.0'}}"
 echo Downloaded RandomNumber.bat.
 pause
