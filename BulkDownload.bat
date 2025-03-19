a3028db26d17a3e2f768b81adf1b037a0f625226
BlakeeBobz <123bobisgaming@gmail.com>
2025-03-19 07:50:03 -0400

    Update BulkDownload.bat

--- BulkDownload.bat
+++ BulkDownload.bat
@@ -2,8 +2,14 @@
 echo Hi
 pause
 cls
-powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/BlakeeBobz/windows.bat/main/CollectUserData.bat' -OutFile 'file.ext'}"
+powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/BlakeeBobz/windows.bat/main/' -OutFile 'CollectUserData.bat'}"
 echo Downloaded CollectUserData.bat.
+powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/BlakeeBobz/windows.bat/main/' -OutFile 'Echo.bat'}"
+echo Downloaded Echo.bat.
+powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/BlakeeBobz/windows.bat/main/' -OutFile 'GetInputAsVariable.bat'}"
+echo Downloaded GetInputAsVariable.bat.
+powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/BlakeeBobz/windows.bat/main/' -OutFile 'GetName.bat'}"
+echo Downloaded GetName.bat.
+powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/BlakeeBobz/windows.bat/main/' -OutFile 'RandomNumber.bat'}"
+echo Downloaded RandomNumber.bat.
 pause
-
-::wip
