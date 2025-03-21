6f4bf428e528270985486c14261b6bf587e27743
BlakeeBobz <123bobisgaming@gmail.com>
2025-03-21 08:42:03 -0400

    Create attactwithmusic.bat

---
+++ attactwithmusic.bat
@@ -0,0 +1,20 @@
+@echo off
+start https://safeshare.tv/x/ONzntmMFXGE
+timeout /t 1 /nobreak >nul
+start https://safeshare.tv/x/ONzntmMFXGE
+timeout /t 1 /nobreak >nul
+start https://safeshare.tv/x/ONzntmMFXGE
+timeout /t 1 /nobreak >nul
+start https://safeshare.tv/x/ONzntmMFXGE
+timeout /t 1 /nobreak >nul
+start https://safeshare.tv/x/ONzntmMFXGE
+timeout /t 1 /nobreak >nul
+start https://safeshare.tv/x/ONzntmMFXGE
+timeout /t 1 /nobreak >nul
+start https://safeshare.tv/x/ONzntmMFXGE
+timeout /t 1 /nobreak >nul
+start https://safeshare.tv/x/ONzntmMFXGE
+timeout /t 1 /nobreak >nul
+start https://safeshare.tv/x/ONzntmMFXGE
+timeout /t 1 /nobreak >nul
+start https://safeshare.tv/x/ONzntmMFXGE
